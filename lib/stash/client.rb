e38cadd0ee542db0cd5252745514faef69e3f9f8
Joshua Westmoreland <Joshua.Westmoreland@theice.com>
2015-05-29 15:04:03 -0400

    Added some methods to expand functionality; updated gemspec to be more specific; bumped version to 0.1.1

--- lib/stash/client.rb
+++ lib/stash/client.rb
@@ -5,91 +5,105 @@ require 'json'
 
 module Stash
   class Client
-
     attr_reader :url
 
+    public
+
     def initialize(opts = {})
-      if opts[:client]
-        @client = opts[:client]
-      else
-        if opts[:host] && opts[:scheme]
-          @url = Addressable::URI.parse(opts[:scheme] + '://' + opts[:host] + '/rest/api/1.0/')
-        elsif opts[:host]
-          @url = Addressable::URI.parse('http://' + opts[:host] + '/rest/api/1.0/')
-        elsif opts[:url]
-          @url = Addressable::URI.parse(opts[:url])
-        elsif opts[:uri] && opts[:uri].kind_of?(Addressable::URI)
-          @url = opts[:uri]
-        else
-          raise ArgumentError, "must provide :url or :host"
-        end
-
-        @url.userinfo = opts[:credentials] if opts[:credentials]
-
-        @client = Faraday.new(@url.site)
-      end
-
+      opts[:client] ? @client = opts[:client] : parse_opts(opts)
     end
 
     def projects
-      fetch_all @url.join('projects')
+      fetch_all(@url.join('projects'))
     end
 
     def create_project(opts={})
-      post @url.join('projects'), opts
+      post(@url.join('projects'), opts)
     end
 
     def update_project(project, opts={})
       relative_project_path = project.fetch('link').fetch('url')
-      put @url.join(remove_leading_slash(relative_project_path)), opts
+      put(@url.join(remove_leading_slash(relative_project_path)), opts)
     end
 
     def delete_project(project)
       relative_project_path = project.fetch('link').fetch('url')
-      delete @url.join(remove_leading_slash(relative_project_path))
+      delete(@url.join(remove_leading_slash(relative_project_path)))
     end
 
     def repositories
       projects.map do |project|
         relative_project_path = project.fetch('link').fetch('url') + '/repos'
-        fetch_all @url.join(remove_leading_slash(relative_project_path))
+        fetch_all(@url.join(remove_leading_slash(relative_project_path)))
       end.flatten
     end
 
+    def repositories_for_project(project)
+      repos = []
+
+      begin
+        project.to_i == 0 ? key = 'name' : key = 'id'
+        repositories.each { |e| repos.push(e) if e['project'][key] == project }
+      rescue Exception => e
+        raise_error(ArgumentError, e, "The input must be either the repository's name or ID")
+      end
+
+      return repos
+    end
+
     def project_named(name)
-      projects.find { |e| e['name'] == name }
+      projects.find { |e| e['name'].eql?(name) }
+    end
+
+    def project_with_key(key)
+      projects.find { |e| e['project']['key'].eql?(key) }
     end
 
     def repository_named(name)
-      repositories.find { |e| e['name'] == name }
+      repositories.find { |e| e['name'].eql?(name) }
+    end
+
+    def repository_with_id(id)
+      repositories.find { |e| e['id'] == id.to_i }
+    end
+
+    def clone_url_for(repo, format = 'ssh')
+      the_url = nil
+
+      unless format.eql?('https') || format.eql?('ssh')
+        raise_error(ArgumentError, '', "The clone url format must be either 'https' or 'ssh'")
+      end
+
+      begin
+        repo.to_i == 0 ? the_repo = repository_named(repo) : the_repo = repository_with_id(repo)
+        the_repo['links']['clone'].each { |e| the_url = e['href'] if i['name'].eql?(format) }
+      rescue Exception => e
+        raise_error(ArgumentError, e, "The input must be either the repository's name or ID")
+      end
+
+      return the_url
     end
 
     def commits_for(repo, opts = {})
       query_values = {}
 
-      path = remove_leading_slash repo.fetch('link').fetch('url').sub('browse', 'commits')
+      path = remove_leading_slash(repo.fetch('link').fetch('url').sub('browse', 'commits'))
       uri = @url.join(path)
 
       query_values['since'] = opts[:since] if opts[:since]
       query_values['until'] = opts[:until] if opts[:until]
       query_values['limit'] = Integer(opts[:limit]) if opts[:limit]
 
-      if query_values.empty?
-        # default limit to 100 commits
-        query_values['limit'] = 100
-      end
+      # default limit to 100 commits
+      query_values['limit'] = 100 if query_values.empty?
 
       uri.query_values = query_values
 
-      if query_values['limit'] && query_values['limit'] < 100
-        fetch(uri).fetch('values')
-      else
-        fetch_all(uri)
-      end
+      query_values['limit'] && query_values['limit'] < 100 ? fetch(uri).fetch('values') : fetch_all(uri)
     end
 
     def changes_for(repo, sha, opts = {})
-      path = remove_leading_slash repo.fetch('link').fetch('url').sub('browse', 'changes')
+      path = remove_leading_slash(repo.fetch('link').fetch('url').sub('browse', 'changes'))
       uri = @url.join(path)
 
       query_values = { 'until' =>  sha }
@@ -98,14 +112,33 @@ module Stash
 
       uri.query_values = query_values
 
-      if query_values['limit']
-        fetch(uri).fetch('values')
-      else
-        fetch_all(uri)
-      end
+      query_values['limit'] ? fetch(uri).fetch('values') : fetch_all(uri)
     end
 
     private
+
+    def parse_opts(opts)
+      if opts[:host] && opts[:scheme]
+        @url = Addressable::URI.parse("#{opts[:scheme]}://#{opts[:host]}/rest/api/1.0/")
+      elsif opts[:host]
+        @url = Addressable::URI.parse("http://#{opts[:host]}/rest/api/1.0/")
+      elsif opts[:url]
+        @url = Addressable::URI.parse(opts[:url])
+      elsif opts[:uri] && opts[:uri].kind_of?(Addressable::URI)
+        @url = opts[:uri]
+      else
+        raise_error(ArgumentError, '', 'must provide :url or :host')
+      end
+
+      @url.userinfo = opts[:credentials] if opts[:credentials]
+      @client = Faraday.new(@url.site)
+    end
+
+    def raise_error(error_type, error_message = '', custom_message = '')
+      message = ''
+      message = "ERROR: #{error_message}\n#{custom_message}" unless error_message.eql('') && custom_message.eql?('')
+      raise(error_type, message)
+    end
 
     def fetch_all(uri)
       response, result = {}, []
@@ -122,57 +155,58 @@ module Stash
     end
 
     def fetch(uri)
-      res = @client.get { |req|
-        req.url uri.to_s
+      res = @client.get do |req|
+        req.url(uri.to_s)
         req.headers['Accept'] = 'application/json'
-      }
+      end
 
       parse(res.body)
     end
 
     def post(uri, data)
-      res = @client.post { |req|
-        req.url uri.to_s
+      res = @client.post do |req|
+        req.url(uri.to_s)
         req.body = data.to_json
 
         req.headers['Content-Type'] = 'application/json'
-        req.headers['Accpet']       = 'application/json'
-      }
+        req.headers['Accpet'] = 'application/json'
+      end
 
       parse(res.body)
     end
 
     def put(uri, data)
-      res = @client.put { |req|
-        req.url uri.to_s
+      res = @client.put do |req|
+        req.url(uri.to_s)
         req.body = data.to_json
 
         req.headers['Content-Type'] = 'application/json'
-        req.headers['Accpet']       = 'application/json'
-      }
+        req.headers['Accpet'] = 'application/json'
+      end
 
       parse(res.body)
     end
 
     def delete(uri)
-      res = @client.delete { |req|
-        req.url uri.to_s
-        req.headers['Accpet']       = 'application/json'
-      }
+      res = @client.delete do |req|
+        req.url(uri.to_s)
+        req.headers['Accpet'] = 'application/json'
+      end
 
       res.body
     end
 
     def parse(str)
-      JSON.parse(str)
-    rescue Encoding::InvalidByteSequenceError
-      # HACK
-      JSON.parse(str.force_encoding("UTF-8"))
+      begin
+        JSON.parse(str)
+      rescue Encoding::InvalidByteSequenceError
+        # HACK
+        JSON.parse(str.force_encoding('UTF-8'))
+      end
     end
 
     def remove_leading_slash(str)
       str.sub(/\A\//, '')
     end
-
   end
 end
--- stash-client.gemspec
+++ stash-client.gemspec
@@ -6,11 +6,11 @@ require 'stash/client/version'
 Gem::Specification.new do |spec|
   spec.name          = "stash-client"
   spec.version       = Stash::Client::VERSION
-  spec.authors       = ["Jari Bakken"]
-  spec.email         = ["jari.bakken@gmail.com"]
-  spec.description   = %q{Atlassian Stash Client}
-  spec.summary       = %q{Atlassian Stash Client}
-  spec.homepage      = "http://github.com/finn-no/stash-client"
+  spec.authors       = ["Jari Bakken", "Josh Westmoreland"]
+  spec.email         = ["jari.bakken@gmail.com", "joshua.westmoreland@theice.com"]
+  spec.description   = %q{Stash Client Gem}
+  spec.summary       = %q{Atlassian Stash Client for Ruby}
+  spec.homepage      = "https://stash.intcx.net/projects/CM/repos/stash-client/browse"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
@@ -18,13 +18,13 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.required_ruby_version = '>= 1.9.2'
+  
 
   spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec"
-  spec.add_development_dependency "webmock"
+  spec.add_development_dependency "rake", "~> 10.4", ">= 10.4.2"
+  spec.add_development_dependency "rspec", "~> 3.2", ">= 3.2.0"
+  spec.add_development_dependency "webmock", "~> 1.21", ">= 1.21.0"
 
-  spec.add_dependency 'faraday'
-  spec.add_dependency 'addressable'
+  spec.add_dependency "faraday", "~> 0.9", ">= 0.9.1"
+  spec.add_dependency "addressable", "~> 2.3", ">= 2.3.8"
 end
